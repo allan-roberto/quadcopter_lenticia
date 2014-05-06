cb3e245f64ba4a4b256225f1a562a77622da38f2
Allan Amorim <allan.amorim@tpv-tech.com>
2014-05-06 15:28:31 -0400

    ignoring objects

--- quadcopter/Release/dev/src/hal/servo.d
+++ quadcopter/Release/dev/src/hal/servo.d
@@ -1,4 +1,4 @@
 dev/src/hal/servo.d: ../dev/src/hal/servo.c \
- /home/allan_amorim/workspace_lenticia/quadcopter_lenticia/quadcopter/dev/include/servo.h
+ /opt/quadcopter_lenticia/quadcopter/dev/include/servo.h
 
-/home/allan_amorim/workspace_lenticia/quadcopter_lenticia/quadcopter/dev/include/servo.h:
+/opt/quadcopter_lenticia/quadcopter/dev/include/servo.h:
