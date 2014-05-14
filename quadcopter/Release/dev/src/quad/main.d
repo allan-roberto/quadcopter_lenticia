938cebd038afb50d07033701ff60952b1163d744
Allan Amorim <allan.amorim@tpv-tech.com>
2014-05-14 17:12:39 -0400

    add gyro log for octave

--- quadcopter/Release/dev/src/quad/main.d
+++ quadcopter/Release/dev/src/quad/main.d
@@ -1,43 +1,43 @@
 dev/src/quad/main.d: ../dev/src/quad/main.c \
- /home/allan_amorim/quadcopter_lenticia/quadcopter/dev/include/lib_xcopter.h \
- /home/allan_amorim/quadcopter_lenticia/quadcopter/dev/include/input.h \
- /home/allan_amorim/quadcopter_lenticia/quadcopter/dev/include/time.h \
- /home/allan_amorim/quadcopter_lenticia/quadcopter/dev/include/uart.h \
- /home/allan_amorim/quadcopter_lenticia/quadcopter/dev/include/gyro.h \
- /home/allan_amorim/quadcopter_lenticia/quadcopter/dev/include/types.h \
- /home/allan_amorim/quadcopter_lenticia/quadcopter/dev/include/binary.h \
- /home/allan_amorim/quadcopter_lenticia/quadcopter/dev/include/def.h \
- /home/allan_amorim/quadcopter_lenticia/quadcopter/dev/include/i2c.h \
- /home/allan_amorim/quadcopter_lenticia/quadcopter/dev/include/wiring.h \
- /home/allan_amorim/quadcopter_lenticia/quadcopter/dev/include/binary.h \
- /home/allan_amorim/quadcopter_lenticia/quadcopter/dev/include/pwm.h \
- /home/allan_amorim/quadcopter_lenticia/quadcopter/dev/include/types.h \
- /home/allan_amorim/quadcopter_lenticia/quadcopter/dev/include/accelerometer.h
+ /opt/quadcopter_lenticia/quadcopter/dev/include/lib_xcopter.h \
+ /opt/quadcopter_lenticia/quadcopter/dev/include/input.h \
+ /opt/quadcopter_lenticia/quadcopter/dev/include/time.h \
+ /opt/quadcopter_lenticia/quadcopter/dev/include/uart.h \
+ /opt/quadcopter_lenticia/quadcopter/dev/include/gyro.h \
+ /opt/quadcopter_lenticia/quadcopter/dev/include/types.h \
+ /opt/quadcopter_lenticia/quadcopter/dev/include/binary.h \
+ /opt/quadcopter_lenticia/quadcopter/dev/include/def.h \
+ /opt/quadcopter_lenticia/quadcopter/dev/include/i2c.h \
+ /opt/quadcopter_lenticia/quadcopter/dev/include/wiring.h \
+ /opt/quadcopter_lenticia/quadcopter/dev/include/binary.h \
+ /opt/quadcopter_lenticia/quadcopter/dev/include/pwm.h \
+ /opt/quadcopter_lenticia/quadcopter/dev/include/types.h \
+ /opt/quadcopter_lenticia/quadcopter/dev/include/accelerometer.h
 
-/home/allan_amorim/quadcopter_lenticia/quadcopter/dev/include/lib_xcopter.h:
+/opt/quadcopter_lenticia/quadcopter/dev/include/lib_xcopter.h:
 
-/home/allan_amorim/quadcopter_lenticia/quadcopter/dev/include/input.h:
+/opt/quadcopter_lenticia/quadcopter/dev/include/input.h:
 
-/home/allan_amorim/quadcopter_lenticia/quadcopter/dev/include/time.h:
+/opt/quadcopter_lenticia/quadcopter/dev/include/time.h:
 
-/home/allan_amorim/quadcopter_lenticia/quadcopter/dev/include/uart.h:
+/opt/quadcopter_lenticia/quadcopter/dev/include/uart.h:
 
-/home/allan_amorim/quadcopter_lenticia/quadcopter/dev/include/gyro.h:
+/opt/quadcopter_lenticia/quadcopter/dev/include/gyro.h:
 
-/home/allan_amorim/quadcopter_lenticia/quadcopter/dev/include/types.h:
+/opt/quadcopter_lenticia/quadcopter/dev/include/types.h:
 
-/home/allan_amorim/quadcopter_lenticia/quadcopter/dev/include/binary.h:
+/opt/quadcopter_lenticia/quadcopter/dev/include/binary.h:
 
-/home/allan_amorim/quadcopter_lenticia/quadcopter/dev/include/def.h:
+/opt/quadcopter_lenticia/quadcopter/dev/include/def.h:
 
-/home/allan_amorim/quadcopter_lenticia/quadcopter/dev/include/i2c.h:
+/opt/quadcopter_lenticia/quadcopter/dev/include/i2c.h:
 
-/home/allan_amorim/quadcopter_lenticia/quadcopter/dev/include/wiring.h:
+/opt/quadcopter_lenticia/quadcopter/dev/include/wiring.h:
 
-/home/allan_amorim/quadcopter_lenticia/quadcopter/dev/include/binary.h:
+/opt/quadcopter_lenticia/quadcopter/dev/include/binary.h:
 
-/home/allan_amorim/quadcopter_lenticia/quadcopter/dev/include/pwm.h:
+/opt/quadcopter_lenticia/quadcopter/dev/include/pwm.h:
 
-/home/allan_amorim/quadcopter_lenticia/quadcopter/dev/include/types.h:
+/opt/quadcopter_lenticia/quadcopter/dev/include/types.h:
 
-/home/allan_amorim/quadcopter_lenticia/quadcopter/dev/include/accelerometer.h:
+/opt/quadcopter_lenticia/quadcopter/dev/include/accelerometer.h:
