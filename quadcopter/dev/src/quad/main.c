318947cfe8c235ff21529e649481c61d2a351890
Allan Amorim <allan.amorim@tpv-tech.com>
2014-05-14 14:26:38 -0400

    generating a data log to be used in octave

--- quadcopter/dev/src/quad/main.c
+++ quadcopter/dev/src/quad/main.c
@@ -15,32 +15,23 @@
 char buffer[40] =  "some characters";
 double data[3];
 uint16_t value = 1000;
+uint16_t i = 0;
 #define UART_NUM 2
 int main(void)
 {
 	uartInit(UART_NUM, UBRR_VAL);
 	init_accelerometer();
 	sei();
-	while(1)
-	{
+	sprintf(buffer, "Accelerometer \n\r");
+	uart_puts (UART_NUM, buffer);
+	for(i=0;i<1000;i++){
 
-		sprintf(buffer, "[0]: %f \n\r",data[0]);
+		sprintf(buffer, "%3.3f %d\r\n",data[2],i);
 		uart_puts (UART_NUM, buffer);
-
-		sprintf(buffer, "[1]: %f \n\r",data[1]);
-		uart_puts (UART_NUM, buffer);
-
-		sprintf(buffer, "[2]: %f \n\r",data[2]);
-		uart_puts (UART_NUM, buffer);
-
-
-		_delay_ms(10);
-		//uart_putc (UART_NUM, 12);
+		_delay_ms(53);
+		_delay_us(270);
 		accelerometer_get_data(&data[0],&data[1],&data[2]);
 	}
-	while(1)
-	{
 
-	}
 	return 0;
 }
