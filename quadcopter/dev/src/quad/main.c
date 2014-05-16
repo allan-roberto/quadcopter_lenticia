01ccd2e46a1f1f37e6cbaaadee7fce3452fad16f
Allan Amorim <allan-roberto.com.br>
2014-05-15 22:58:12 -0400

    ...

--- quadcopter/dev/src/quad/main.c
+++ quadcopter/dev/src/quad/main.c
@@ -15,19 +15,17 @@
 char buffer[40] =  "some characters";
 int16_t giro_data[3];
 uint16_t i = 0;
+#define UART_NUM 2
 int main(void)
 {
-	uartInit(UBRR_VAL);
+	uartInit(UART_NUM,UBRR_VAL);
 	init_gyro();
 	sei();
-	sprintf(buffer, "Gyroscope");
-	uart_puts (buffer);
-	uart_puts ("\n\r");
-	for(i=0;i<1000;i++){
+	for(i=0;i<10000;i++){
 
 		gyro_get_data(&giro_data[0],&giro_data[1],&giro_data[2]);
-		sprintf(buffer, " %d %d\r\n",giro_data[0],i);
-		uart_puts (buffer);
+		sprintf(buffer, " %d %d %d %d\r\n",i, giro_data[0], giro_data[1], giro_data[2]);
+		uart_puts (UART_NUM,buffer);
 		_delay_ms(55);
 		_delay_us(290);
 	}
